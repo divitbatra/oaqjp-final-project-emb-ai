ee82b4c3f81794ce27918332de5715c7b5505bda
Divit Batra <divitbatra@hotmail.com>
2023-11-18 16:50:47 -0700

    error-handling 3

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,10 +8,10 @@ def emotion_detector(text_to_analyse):
     myobj = {"raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
     dominant_emotion_score = 0
     dominant_emotion = ''
     if response.status_code == 200:
+        emotion_dict = formatted_response['emotionPredictions'][0]['emotion']
         for emotion in emotion_dict:
             if dominant_emotion_score < emotion_dict[emotion]:
                 dominant_emotion_score = emotion_dict[emotion]
